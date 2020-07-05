2ab975cd15d2dac24f6ce22fbe82a202fa2d74dc
The German <quincejozbruer@gmail.com>
2020-07-04 21:07:20 -0700

    Sound Check and Selection done

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('keypress', controller)
     function controller(e){
         const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`) 
+        const key = document.querySelector(`kbd[data-key="${e.keyCode}"]`)
         if (!audio) return
         audio.currentTime = 0
         audio.play()
+        
+        key.classList.add('playing')
+        setTimeout(() => {
+
+        }, 0.7)
     }
 
     
